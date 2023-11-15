8a053cb054e7a5b9dd1071abbcbe4e025c2132d5
oeyoews <jyao4783@gmail.com>
2023-11-15 16:52:22 +0800

    Update Nav.tsx

--- app/ui/Nav.tsx
+++ app/ui/Nav.tsx
@@ -5,10 +5,10 @@ export default function Nav() {
   return (
     <>
       <div className="flex items-center mx-auto justify-end print:hidden mb-8 sticky top-0 left-0 right-0 z-[1000] backdrop-blur-sm p-4 bg-white/30 max-w-4xl space-x-4 ">
-        <Link href="/">
+        <Link href="/" title="home">
           <FaHome className="h-5 w-5" />
         </Link>
-        <Link href="/song">
+        <Link href="/song" title="song">
           <FaMusic className="h-5 w-5" />
         </Link>
       </div>
