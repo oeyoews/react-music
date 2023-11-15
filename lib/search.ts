380d172eb8f78e29228000a2cdb99e1a5b951940
oeyoews <jyao4783@gmail.com>
2023-11-15 09:42:06 +0800

    update

--- lib/search.ts
+++ lib/search.ts
@@ -1,9 +1,14 @@
-const baseurl = process.env.NEXT_PUBLIC_MUSIC_API;
+// https://github.com/vercel/next.js/discussions/48324
+// cache: 'no-cache', // 仍然提示 fetch for over 2MB of data can not be cached
 
+// 创建一个工具函数，用于生成完整的 API 地址
+function createApiUrl(endpoint: string): string {
+  return `${process.env.NEXT_PUBLIC_MUSIC_API}/${endpoint}`;
+}
+
+// 重构每个函数，使用 createApiUrl 生成完整的 API 地址
 export async function getMusicURL(id: number): Promise<MusicURL> {
-  const res = await fetch(`${baseurl}/song/url?id=${id}`, {
-    // https://github.com/vercel/next.js/discussions/48324
-    // cache: 'no-cache', // 仍然提示 fetch for over 2MB of data can not be cached
+  const res = await fetch(createApiUrl(`song/url?id=${id}`), {
     cache: 'force-cache',
     credentials: 'include',
   });
@@ -12,35 +17,23 @@ export async function getMusicURL(id: number): Promise<MusicURL> {
 }
 
 export async function getPlayList(id: number): Promise<PlayListDetails> {
-  const res = await fetch(`${baseurl}/playlist/detail?id=${id}`, {
+  const res = await fetch(createApiUrl(`playlist/detail?id=${id}`), {
     credentials: 'include',
   });
   const data = await res.json();
   return data;
 }
 
-/**
- * Searches for a specific set of keywords.
- *
- * @param {string} keywords - The keywords to search for.
- * @return {Promise<Search>} - A promise that resolves to the search results.
- */
 export async function search(keywords: string): Promise<Search> {
-  const res = await fetch(`${baseurl}/search?keywords=${keywords}`, {
+  const res = await fetch(createApiUrl(`search?keywords=${keywords}`), {
     credentials: 'include',
   });
   const data = await res.json();
   return data;
 }
 
-/**
- * Searches for the top songs of an artist based on their ID.
- *
- * @param {number} id - The ID of the artist.
- * @return {Promise<SearchTop>} A Promise that resolves to the top songs data.
- */
 export async function searchTop(id: number): Promise<SearchTop> {
-  const res = await fetch(`${baseurl}/artist/top/song?id=${id}`, {
+  const res = await fetch(createApiUrl(`artist/top/song?id=${id}`), {
     credentials: 'include',
   });
   const data = await res.json();
