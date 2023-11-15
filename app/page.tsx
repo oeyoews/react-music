eb2bc4e68707f653fc2060c3ad3d6d757702d114
oeyoews <jyao4783@gmail.com>
2023-11-15 12:30:57 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default async function Home() {
   // const lyric = await getSongLyric(28798452);
 
   return (
-    <div className='prose mx-auto max-w-4xl'>
+    <div className='prose mx-auto max-w-4xl p-1'>
       {/* <audio controls>
         <source src={data?.[0].url} type='audio/mpeg' />
       </audio> */}
@@ -51,58 +51,49 @@ export default async function Home() {
       </div>
 
       {/* recommend songs */}
-      <h2>推荐歌曲</h2>
-      <hr />
-      <ol className='columns-2'>
-        {dailySongs.map(({ name, id, recommendReason }) => (
-          <div className='flex space-x-2 items-center' key={id}>
-            <li>{name}</li>
-            <div className=''>
-              {recommendReason && <div> -- {recommendReason}</div>}
+      <div className='mx-1'>
+        <h2>推荐歌曲</h2>
+        <hr />
+        <ol className='columns-1 md:columns-2'>
+          {dailySongs.map(({ name, id, recommendReason }) => (
+            <div className='flex space-x-2 items-center' key={id}>
+              <li>{name}</li>
+              <div className=''>
+                {recommendReason && <div> -- {recommendReason}</div>}
+              </div>
             </div>
-          </div>
-        ))}
-      </ol>
+          ))}
+        </ol>
+      </div>
 
-      <h2>热门歌单</h2>
-      <hr />
-      <ol className='space-y-2 columns-2'>
-        {playlists.map((playlist) => (
-          <div className='flex items-center space-x-2' key={playlist.name}>
-            <li className='not-prose'>
-              <Image
-                src={playlist.coverImgUrl}
-                alt={playlist.name}
-                width={22}
-                height={22}
-                className='rounded-full'
-              />
-            </li>
-            <div>{playlist.name}</div>
-          </div>
-        ))}
-      </ol>
-
-      <h2>热门歌曲</h2>
-      <hr />
-      <ol className='columns-2'>
-        {songstop.data.map((song) => (
-          <div className='flex flex-row' key={song.searchWord}>
-            {/* {song.iconUrl && (
-              <div>
+      <div className='mx-1'>
+        <h2>热门歌单</h2>
+        <hr />
+        <ol className='space-y-2 columns-1 md:columns-2'>
+          {playlists.map((playlist) => (
+            <div className='flex items-center space-x-2' key={playlist.name}>
+              <li className='not-prose'>
                 <Image
-                  src={song.iconUrl as string}
-                  alt={song.searchWord}
+                  src={playlist.coverImgUrl}
+                  alt={playlist.name}
                   width={22}
                   height={22}
                   className='rounded-full'
                 />
-              </div>
-            )} */}
-            <li key={song.searchWord}>
-              {song.searchWord}
-              {song.content && <div> -- {song.content}</div>}
-            </li>
+              </li>
+              <div>{playlist.name}</div>
+            </div>
+          ))}
+        </ol>
+      </div>
+
+      <h2>热门歌曲</h2>
+      <hr />
+      <ol className='columns-1 md:columns-2'>
+        {songstop.data.map((song) => (
+          <div className='flex items-center' key={song.searchWord}>
+            <li key={song.searchWord}>{song.searchWord}</li>
+            {song.content && <div> -- {song.content}</div>}
           </div>
         ))}
       </ol>
