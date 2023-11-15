263b6f8201d7b590cc4f3d2a3da1946d18f32b2b
oeyoews <jyao4783@gmail.com>
2023-11-15 14:40:41 +0800

    update

--- README.md
+++ README.md
@@ -11,6 +11,8 @@ https://nextjs-netease-music.vercel.app/
 - download url not available
 - recent song
 
+<!-- https://github.com/imsyy/SPlayer/blob/dev/src/api/home.js -->
+
 ```
 Error: ENOENT: no such file or directory, scandir 'D:\Workspace\react-music\.next\server\vendor-chunks\module'
 ```
--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  crossOrigin: 'anonymous',
+  // https://github.com/vercel/next.js/discussions/34810
+  // https://nextjs.org/docs/architecture/nextjs-compiler
+  compiler: {
+    removeConsole:
+      process.env.NODE_ENV === 'production' ? { exclude: ['error'] } : false,
+  },
   images: {
     remotePatterns: [
       {
