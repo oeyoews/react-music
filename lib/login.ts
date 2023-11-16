36d3e918d6ed9e185b78f218c7567359886acd84
oeyoews <jyao4783@gmail.com>
2023-11-16 11:02:52 +0800

    Update login.ts

--- lib/login.ts
+++ lib/login.ts
@@ -2,11 +2,19 @@
 import { createApiUrl } from './createApiUrl';
 import { fetchData } from './fetchData';
 
+/**
+ * generate qr code key
+ */
 export const getqrKey = async (): Promise<IQRCode> => {
   const url = createApiUrl('/login/qr/key', { timestamp: Date.now() });
   return await fetchData(url);
 };
 
+/**
+ * generate qrcode image
+ * @param key
+ * @returns
+ */
 export const qrCreate = async (key: string): Promise<IQRCodeData> => {
   const url = createApiUrl('/login/qr/create', {
     key,
@@ -16,6 +24,11 @@ export const qrCreate = async (key: string): Promise<IQRCodeData> => {
   return await fetchData(url);
 };
 
+/**
+ * check qrcode status
+ * @param key
+ * @returns
+ */
 export const qrCheck = async (key: string): Promise<CheckQrcode> => {
   const url = createApiUrl('/login/qr/check', {
     key,
@@ -24,12 +37,25 @@ export const qrCheck = async (key: string): Promise<CheckQrcode> => {
   return await fetchData(url, { method: 'post' });
 };
 
+/**
+ * check login status
+ * @returns
+ */
 export const getQrStatus = async (): Promise<ILoginStatus> => {
   const url = createApiUrl('/login/status', { timestamp: Date.now() });
   return await fetchData(url);
 };
 
+/**
+ * login with anonymous
+ * @returns
+ */
 export const loginAnonymous = async (): Promise<any> => {
   const url = createApiUrl('/register/anonymous', { timestamp: Date.now() });
+  return await fetchData(url);
+};
+
+export const refreshLogin = async () => {
+  const url = createApiUrl('/login/refresh', { timestamp: Date.now() });
   return await fetchData(url);
 };
