6c920d5a18c6194b313bcc06f776866363151556
oeyoews <jyao4783@gmail.com>
2023-11-17 21:27:02 +0800

    update

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,4 +1,18 @@
+import { Metadata } from 'next';
 import Search from '~app/ui/Search';
+
+export const metadata: Metadata = {
+  title: 'Search',
+  description: 'Search for songs, artists, or albums',
+};
+
+export const generatestaticParams = async () => {
+  return [
+    {
+      slug: 'search',
+    },
+  ];
+};
 
 export default function page() {
   return <Search />;
