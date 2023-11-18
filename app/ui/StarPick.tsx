8feed9bccbe9a56100e2dd1468d18d1188f3ad70
oeyoews <jyao4783@gmail.com>
2023-11-18 16:02:06 +0800

    update

--- app/ui/StarPick.tsx
+++ app/ui/StarPick.tsx
@@ -27,38 +27,38 @@ export default function StarPick() {
   return (
     <div className="m-2">
       <h2>云村星评</h2>
-      {!loading && starPick?.blocks ? (
-        starPick.blocks.map((block) => {
-          return block.creatives?.slice(0, 9).map((creative) => {
-            const resources = creative.resources[0];
-            const { songData, users } = resources.resourceExtInfo;
-            return (
-              <div key={creative.creativeId}>
-                <div className="m-2 flex justify-between p-2 flex-wrap">
-                  <div
-                    title={users[0].userId.toString()}
-                    className="w-full md:w-auto">
-                    <div className="flex items-center space-x-2">
-                      <div>{users[0].nickname}</div>
-                      <Avatar userData={users[0]} />
+      <div className="grid gap-8 grid-cols-1 md:grid-cols-3">
+        {!loading && starPick?.blocks ? (
+          starPick.blocks.map((block) => {
+            return block.creatives?.slice(0, 9).map((creative) => {
+              const resources = creative.resources[0];
+              const { songData, users } = resources.resourceExtInfo;
+              return (
+                <Link
+                  href={`/song/${songData.id}`}
+                  key={creative.creativeId}
+                  className="hover:scale-105 transition-all">
+                  <div className="w-full h-full m-2 flex justify-between p-2 flex-wrap bg-neutral-100 rounded-md shadow">
+                    <div
+                      title={users[0].userId.toString()}
+                      className="w-full md:w-auto">
+                      <div className="line-clamp-3">
+                        {resources.uiElement.mainTitle.titleDesc} --
+                      </div>
                     </div>
-                    {resources.uiElement.mainTitle.titleDesc} --
+                    <div className="flex items-center space-x-2 w-full justify-end">
+                      <Avatar userData={users[0]} />
+                      <div>{users[0].nickname}</div>
+                    </div>
                   </div>
-                  <div className="w-full md:auto text-right">
-                    <Link href={`/song/${songData.id}`}>
-                      {songData.name} --
-                      {songData.artists[0].name}
-                    </Link>
-                  </div>
-                </div>
-                <hr />
-              </div>
-            );
-          });
-        })
-      ) : (
-        <div className="h-24 bg-neutral-100 animate-pulse">.</div>
-      )}
+                </Link>
+              );
+            });
+          })
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 }
