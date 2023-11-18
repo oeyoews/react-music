7f762adbc771a16fa8006c27f4858c8df9666ccd
oeyoews <jyao4783@gmail.com>
2023-11-18 16:48:20 +0800

    feat: update spinner

--- app/ui/RecommendSongs.tsx
+++ app/ui/RecommendSongs.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 import { getRecommendations } from '~lib/search';
 import Link from 'next/link';
+import Spinner from './Spinner';
 
 export default function RecommendSongs() {
   const [data, setData] = useState<DailySong[]>();
@@ -12,7 +13,6 @@ export default function RecommendSongs() {
   useEffect(() => {
     if (!localStorage.cookie) {
       setHasCookie(false);
-      // return;
     }
     getRecommendations(localStorage.cookie).then((recommendSongs) => {
       setData(recommendSongs.data.dailySongs);
@@ -20,31 +20,32 @@ export default function RecommendSongs() {
     });
   }, []);
 
+  const content = (
+    <ol className="columns-1 md:columns-2">
+      {hasCookie &&
+        data?.map(({ name, id, recommendReason }) => (
+          <li key={id}>
+            <Link href={`/song/${id}`}>
+              <div className="flex space-x-2 items-center">
+                <div>{name}</div>
+                {recommendReason && <div> -- {recommendReason}</div>}
+              </div>
+            </Link>
+          </li>
+        ))}
+    </ol>
+  );
+
   return (
     <div>
       <h2>每日推荐</h2>
-      <ol className="columns-1 md:columns-2">
-        {/* TODO: 缓存 */}
-        {hasCookie && data ? (
-          data.map(({ name, id, recommendReason }) => (
-            <li key={id}>
-              <Link href={`/song/${id}`}>
-                <div className="flex space-x-2 items-center">
-                  <div>{name}</div>
-                  {recommendReason && <div> -- {recommendReason}</div>}
-                </div>
-              </Link>
-            </li>
-          ))
-        ) : (
-          <div>
-            {loading && (
-              <div className="h-24 bg-neutral-100 rounded animate-pulse"></div>
-            )}
-            {!hasCookie && <div>需要登录</div>}
-          </div>
-        )}
-      </ol>
+      {content}
+      {loading && (
+        <div className="flex justify-center items-center">
+          <Spinner />
+        </div>
+      )}
+      {!hasCookie && <div>需要登录</div>}
     </div>
   );
 }
--- app/ui/StarPick.tsx
+++ app/ui/StarPick.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Avatar from './Avatar';
 import { loginAnonymous } from '~lib/login';
+import Spinner from './Spinner';
 
 export default function StarPick() {
   const [starPick, setStarPick] = useState<StarPick>();
@@ -26,35 +27,31 @@ export default function StarPick() {
 
   const content = (
     <div className="grid gap-8 grid-cols-1 md:grid-cols-3">
-      {!loading && starPick?.blocks ? (
-        starPick.blocks.map((block) => {
-          return block.creatives?.slice(0, 9).map((creative) => {
-            const resources = creative.resources[0];
-            const { songData, users } = resources.resourceExtInfo;
-            return (
-              <Link
-                href={`/song/${songData.id}`}
-                key={creative.creativeId}
-                title={songData.name}
-                className="hover:scale-105 transition-all duration-500">
-                <div className="w-full h-full m-2 flex justify-between p-2 flex-wrap bg-neutral-100 rounded-md shadow">
-                  <div className="w-full md:w-auto">
-                    <div className="line-clamp-3">
-                      {resources.uiElement.mainTitle.titleDesc} --
-                    </div>
-                  </div>
-                  <div className="flex items-center space-x-2 w-full justify-end">
-                    <Avatar userData={users[0]} />
-                    <div>{users[0].nickname}</div>
+      {starPick?.blocks.map((block) => {
+        return block.creatives?.slice(0, 9).map((creative) => {
+          const resources = creative.resources[0];
+          const { songData, users } = resources.resourceExtInfo;
+          return (
+            <Link
+              href={`/song/${songData.id}`}
+              key={creative.creativeId}
+              title={songData.name}
+              className="hover:scale-105 transition-all duration-500">
+              <div className="w-full h-full m-2 flex justify-between p-2 flex-wrap bg-neutral-100 rounded-md shadow">
+                <div className="w-full md:w-auto">
+                  <div className="line-clamp-3">
+                    {resources.uiElement.mainTitle.titleDesc} --
                   </div>
                 </div>
-              </Link>
-            );
-          });
-        })
-      ) : (
-        <></>
-      )}
+                <div className="flex items-center space-x-2 w-full justify-end">
+                  <Avatar userData={users[0]} />
+                  <div>{users[0].nickname}</div>
+                </div>
+              </div>
+            </Link>
+          );
+        });
+      })}
     </div>
   );
 
@@ -62,6 +59,11 @@ export default function StarPick() {
     <div className="m-2">
       <h2>云村星评</h2>
       {content}
+      {loading && (
+        <div className="flex justify-center">
+          <Spinner />
+        </div>
+      )}
     </div>
   );
 }
