d15d38f3bccb78f55f914f8bb9f4aa0c47ad38d7
oeyoews <jyao4783@gmail.com>
2023-11-24 22:23:31 +0800

    Update route.ts

--- app/api/[slug]/route.ts
+++ app/api/[slug]/route.ts
@@ -71,3 +71,69 @@ export async function POST(request: Request, { params }: { params: Params }) {
     },
   );
 }
+
+export async function GET(request: Request, { params }: { params: Params }) {
+  const res = await request.json();
+
+  const { slug } = params;
+
+  const { id, cookie } = res;
+
+  const recommendSongs = await app.recommend_songs({
+    cookie,
+  });
+
+  const artistDetailData = await app.artist_detail({
+    id,
+  });
+
+  const searchData = await app.search({
+    keywords: res.keywords,
+  });
+
+  const mvCommentData = await app.comment_mv({
+    id: res.mvid,
+    limit: 99,
+  });
+
+  const mvDetailData = (await app.mv_detail({
+    mvid: res.mvid,
+  })) as unknown as { status: number; body: IMvDetail };
+
+  let data;
+
+  if (slug === 'recommend_songs') {
+    data = recommendSongs.body as unknown as IRecommendSongs;
+  }
+
+  if (slug === 'artist_detail') {
+    data = artistDetailData.body as unknown as IArtistDetail;
+  }
+
+  if (slug === 'search') {
+    data = searchData.body as unknown as ISearch;
+  }
+
+  if (slug === 'mv_detail') {
+    data = mvDetailData.body;
+  }
+
+  if (slug === 'mv_comment') {
+    data = mvCommentData.body;
+  }
+
+  console.log('当前api:', slug);
+  console.log(slug, '-> 数据:', data);
+
+  // 支持跨域
+  return Response.json(
+    { data },
+    {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+    },
+  );
+}
