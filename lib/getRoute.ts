d01ad2bc2d3ae86110b7f93f7540da6a7a35709b
oeyoews <jyao4783@gmail.com>
2023-12-02 21:01:59 +0800

    update

--- lib/getRoute.ts
+++ lib/getRoute.ts
@@ -1,23 +1,22 @@
 import { Route } from 'next';
-export const getRoute = (banner: Banner): Route => {
+
+enum TargetType {
+  song = 1,
+  album = 10,
+  playlist = 1000,
+  mv = 1004,
+  external = 3000,
+}
+
+export const getRoute = (banner: Banner) => {
   const { targetId, targetType, url } = banner;
-  switch (targetType) {
-    case 1:
-      // 歌曲页
-      return `/song/${targetId}`;
-    case 10:
-      // 专辑页
-      return `/album/${targetId}`;
-    case 1000:
-      // 歌单页
-      return `/playlist/${targetId}`;
-    case 1004:
-      // MV页
-      return `/video/${targetId}`;
-    case 3000:
-      // 站外链接
-      return url as string;
-    default:
-      return '';
-  }
+  const targetMap: Record<number, Route> = {
+    [TargetType.song]: `/song/${targetId}`,
+    [TargetType.album]: `/album/${targetId}`,
+    [TargetType.playlist]: `/playlist/${targetId}`,
+    [TargetType.mv]: `/mv/${targetId}`, // video
+    [TargetType.external]: url as string,
+  };
+
+  return targetMap[targetType];
 };
