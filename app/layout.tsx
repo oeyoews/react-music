c75aca7407530ac75640c5f4c9d735e8e8c6a2d7
oeyoews <jyao4783@gmail.com>
2023-12-05 17:35:08 +0800

    fix: support toploader again

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
 import '~app/globals.css';
 import Nav from '~components/Nav/Nav';
+import NextTopLoader from 'nextjs-toploader';
 import { Sidebar } from '~components/Nav/Sidebar';
 import { Toaster } from 'react-hot-toast';
 import clsx from 'clsx';
@@ -15,6 +16,17 @@ export const metadata: Metadata = {
   referrer: 'no-referrer', // 针对一些防盗链
 };
 
+export const viewport: Viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1.5,
+  userScalable: true,
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -25,6 +37,7 @@ export default function RootLayout({
       <body className={clsx('antialiased min-h-screen', inter.className)}>
         <Toaster />
         <Nav />
+        <NextTopLoader color="#eb6864" height={1} />
         <Sidebar />
         <div className="mt-6 prose ml-2 md:ml-56 mr-2 max-w-none px-2">
           {children}
