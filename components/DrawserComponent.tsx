0b7dde2a28108cebfee37bb1dcf477b5251496e2
oeyoews <jyao4783@gmail.com>
2023-12-06 13:15:10 +0800

    update

--- components/DrawserComponent.tsx
+++ components/DrawserComponent.tsx
@@ -12,7 +12,9 @@ export default function DrawserComponent({
   return (
     <Drawer.Root shouldScaleBackground>
       <Drawer.Trigger asChild>
-        <button className="bg-zinc-200 rounded px-2 py-1 mx-2">{text}</button>
+        <button className="bg-zinc-200 rounded px-2 py-1 mx-2 my-2">
+          {text}
+        </button>
       </Drawer.Trigger>
       <Drawer.Portal>
         {/* NOTE: aplayer 的z-index 过高 */}
