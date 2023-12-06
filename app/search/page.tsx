fea397172be366d001bdaa001cb9aae3894d41d5
oeyoews <jyao4783@gmail.com>
2023-12-06 14:18:42 +0800

    update

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,15 +3,18 @@
 import Search from '~components/Search';
 import HotSongs from '~components/HotSongs';
 import { searchHotDetail } from '~lib/search';
-import useSWRImmutable from 'swr/immutable';
 import useSWR from 'swr';
+import SkeletonSongs from '~components/ui/SkeletonSongs';
 
-// TODO: more element
 export default function page() {
   const HotSongComponent = () => {
     const { data, isLoading } = useSWR('/search/hot/detail', searchHotDetail);
 
-    return !isLoading && <HotSongs data={data?.data!} />;
+    return isLoading ? (
+      <SkeletonSongs count={20} />
+    ) : (
+      <HotSongs data={data?.data!} />
+    );
   };
 
   return (
