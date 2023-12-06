7e29a967ad40dcc7cce5eb6f5043b658988b6235
oeyoews <jyao4783@gmail.com>
2023-12-06 15:21:34 +0800

    fix: dynamic not support

--- components/ui/SkeletonAvatar.tsx
+++ components/ui/SkeletonAvatar.tsx
@@ -1,12 +1,13 @@
 import clsx from 'clsx';
 
-export default function SkeletonAvatar({ size = 22 }: { size?: number }) {
+export default function SkeletonAvatar() {
+  // 动态的size 不支持???
   return (
     <div
       className={clsx(
         'bg-gray-300 rounded-full animate-pulse shadow-lg my-0',
-        `w-[${size}px]`,
-        `h-[${size}px]`,
+        `w-[22px]`,
+        `h-[22px]`,
       )}
     />
   );
