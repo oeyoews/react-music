78c2fa66b6f8caf5c9cb4ab62451797503f0e671
oeyoews <jyao4783@gmail.com>
2023-12-06 14:54:36 +0800

    update

--- components/ui/SkeletonAvatar.tsx
+++ components/ui/SkeletonAvatar.tsx
@@ -1,12 +1,13 @@
 import clsx from 'clsx';
+
 export default function SkeletonAvatar({ size = 22 }: { size?: number }) {
   return (
     <div
       className={clsx(
         'bg-gray-300 rounded-full animate-pulse shadow-lg my-0',
         `w-${size}`,
-      )}>
-      SkeletonAvatar
-    </div>
+        `h-${size}`,
+      )}
+    />
   );
 }
