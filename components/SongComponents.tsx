fe48880fd4b5b6ca6bdbdfc9c207cef13f287f9c
oeyoews <jyao4783@gmail.com>
2023-12-07 15:14:43 +0800

    update

--- components/SongComponents.tsx
+++ components/SongComponents.tsx
@@ -37,7 +37,7 @@ export const ArtistMVS = ({ slug }: { slug: string }) => {
   return <MV data={artistMVs?.mvs!} />;
 };
 
-// NOTE: 点击链接后, 路由没有发生变化, 所以 aplayer不会自动卸载
+// TODO: 点击链接后, 路由没有发生变化, 所以 aplayer不会自动卸载, 之后即使切换路由也不会自动卸载
 export const SimiSong = ({ slug }: { slug: string }) => {
   const { data, isLoading } = useSiMiSong(slug);
   return (
