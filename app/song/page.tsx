b8f55dea042f4bffcf82f6a5e0e1339bbbdb5870
oeyoews <jyao4783@gmail.com>
2023-12-08 19:17:59 +0800

    Update page.tsx

--- app/song/page.tsx
+++ app/song/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import SongPage from '~components/SongComponents';
 
 export default async function Page({
@@ -7,5 +8,9 @@ export default async function Page({
 }) {
   // 注意: vip 也会false, 需要带上localstorage, 可以放在songpage 里面进行判断, 或者使用rsc 传递localstorage to server
   // TOD: check wrong url
-  return <SongPage id={searchParams.id} />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SongPage id={searchParams.id} />;
+    </Suspense>
+  );
 }
