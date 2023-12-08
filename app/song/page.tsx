f011873b4ac72bf4935e923d4239a9189e8acd93
oeyoews <jyao4783@gmail.com>
2023-12-08 19:20:32 +0800

    update

--- app/song/page.tsx
+++ app/song/page.tsx
@@ -8,9 +8,5 @@ export default async function Page({
 }) {
   // 注意: vip 也会false, 需要带上localstorage, 可以放在songpage 里面进行判断, 或者使用rsc 传递localstorage to server
   // TOD: check wrong url
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <SongPage id={searchParams.id} />;
-    </Suspense>
-  );
+  return <SongPage id={searchParams.id} />;
 }
