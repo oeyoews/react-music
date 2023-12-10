d76105b6ad836a648396a2f038952e1edbc780d3
oeyoews <jyao4783@gmail.com>
2023-12-10 18:11:58 +0800

    formate code

--- components/SongComponents.tsx
+++ components/SongComponents.tsx
@@ -1,5 +1,6 @@
 'use client';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+import { useSearchParams } from 'next/navigation';
 
 import DrawserComponent from '~components/DrawserComponent';
 import {
